1fa716d180a8c8ea160fa65414ef45d560ea080a
Navas <gabrielnavas@protonmail.com>
2024-10-22 16:54:01 -0300

    manual module

--- conceitos_nest/src/app/app.module.ts
+++ conceitos_nest/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConceitoManualModule } from 'src/conceitos-manual/conceitos-manual.module';
 
 @Module({
-  imports: [],
+  imports: [ConceitoManualModule],
   controllers: [AppController],
   providers: [AppService],
 })
