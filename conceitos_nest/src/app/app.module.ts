1aa672751847b67090b8a0458eae496c3909de32
Navas <gabrielnavas@protonmail.com>
2024-10-22 16:56:20 -0300

    auto module

--- conceitos_nest/src/app/app.module.ts
+++ conceitos_nest/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { ConceitoManualModule } from 'src/conceitos-manual/conceitos-manual.module';
+import { ConceitosAutomaticoModule } from 'src/conceitos-automatico/conceitos-automatico.module';
 
 @Module({
-  imports: [ConceitoManualModule],
+  imports: [
+    ConceitoManualModule,
+    ConceitosAutomaticoModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
