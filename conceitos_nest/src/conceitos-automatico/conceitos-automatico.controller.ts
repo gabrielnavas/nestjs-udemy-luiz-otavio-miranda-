da6db8c25372866deaea03fe00b08b800cd1e85f
Navas <gabrielnavas@protonmail.com>
2024-10-22 18:39:12 -0300

    conceito automatico e manual controller

---
+++ conceitos_nest/src/conceitos-automatico/conceitos-automatico.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('conceitos-automatico')
+export class ConceitosAutomaticoController {
+
+  @Get()
+  getConceitosAutomatico(): string {
+    return 'conceito automatico'
+  }
+}
