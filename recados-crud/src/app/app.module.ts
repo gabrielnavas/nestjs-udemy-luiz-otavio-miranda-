6de10a8addaa4e74d962425d066d4653eb025519
Navas <gabrielnavas@protonmail.com>
2024-10-26 12:17:44 -0300

    add env variables

--- recados-crud/src/app/app.module.ts
+++ recados-crud/src/app/app.module.ts
@@ -13,30 +13,32 @@ import { SimpleMiddleware } from 'src/common/middlewares/simple.middlewares';
 import { AnotherMiddleware } from 'src/common/middlewares/another.middlewares';
 import { APP_GUARD } from '@nestjs/core';
 import { IsAdminGuard } from 'src/common/guards/is-admin.guard';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(), // carregar o .env
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: process.env.DATABASE_TYPE as any,
 
       // deve ser usado o .env
-      host: 'localhost',
-      port: 5432,
-      database: 'postgres',
-      username: 'postgres',
-      password: 'postgres123',
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      database: process.env.PG_DATABASE,
+      username: process.env.PG_USERNAME,
+      password: process.env.PG_PASSWORD,
 
       // carrega entidades sem precisar especifica-las
       // não precisa especificar as entidades no app module
-      autoLoadEntities: true,
+      autoLoadEntities: process.env.NESTJS_AUTO_LOAD_ENTITIES === 'true',
 
       // sincroniza as tabelas com o banco de dados.
       // não deve ser usado em produção
-      synchronize: true,
+      synchronize: process.env.NESTJS_SYNCHRONIZE_ORM === 'true',
 
       // habilita o log de queries SQL no console
       // não usar em produção
-      logging: true,
+      logging: process.env.NESTJS_LOG_SQL === 'true',
     }),
     PessoasModule,
     RecadosModule,
