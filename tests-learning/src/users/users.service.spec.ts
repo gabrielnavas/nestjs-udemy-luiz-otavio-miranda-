daeb82751877cb4b4035cd4d773f2fa188741340
Navas <gabrielnavas@protonmail.com>
2024-10-29 22:14:34 -0300

    create user test success

--- tests-learning/src/users/users.service.spec.ts
+++ tests-learning/src/users/users.service.spec.ts
@@ -1,6 +1,8 @@
 import { HashingService } from 'src/auth/hashing/hashing.service';
 import { UsersService } from './users.service';
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Policy } from 'src/auth/enums/route-policies.enum';
 
 describe('UsersService', () => {
   let userService: UsersService;
@@ -12,7 +14,9 @@ describe('UsersService', () => {
         UsersService,
         {
           provide: HashingService,
-          useValue: {},
+          useValue: {
+            hash: jest.fn().mockResolvedValue('HASH_PASSWORD'),
+          },
         },
       ],
     }).compile();
@@ -21,7 +25,39 @@ describe('UsersService', () => {
   });
 
   it('should be defined', () => {
-    expect(userService).toBeDefined()
-    expect(hashingService).toBeDefined()
+    expect(userService).toBeDefined();
+    expect(hashingService).toBeDefined();
+  });
+
+  describe('createUser success', () => {
+    it('should create a new user', async () => {
+      //  Arrange
+      const createUserDto: CreateUserDto = {
+        email: 'test@example.com',
+        password: '12345678',
+      };
+      jest.spyOn(hashingService, 'hash').mockResolvedValue('HASH_PASSWORD')
+
+      //  Act
+      const result = await userService.createUser({
+        email: 'test@example.com',
+        password: '12345678',
+      });
+
+      //   Assert
+      expect(hashingService.hash).toHaveBeenCalledTimes(1);
+      expect(hashingService.hash).toHaveBeenCalledWith(createUserDto.password);
+
+      expect(result).toBeDefined();
+      expect(typeof result.id === 'string').toBeTruthy();
+      expect(result.email).toEqual(createUserDto.email);
+      expect(result.policies).toEqual([
+        Policy.user,
+        Policy.findUserById,
+        Policy.deleteUser,
+        Policy.findAllUsers,
+        Policy.findUsers,
+      ]);
+    });
   });
 });
